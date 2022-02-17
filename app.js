ec53d7230afb3f4b87e0407dc3988c3f318c5690
Erkki Lepre <erkki@refined.com>
2022-02-17 08:32:45 +0200

    WIP

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Hello, World!! Updated update 3');
+  res.end('Hello, World!! Other');
 }
 
 const server = http.createServer(requestListener);
