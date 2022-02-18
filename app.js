cef18eab3bdb314f8fc14b70b05f39d5f8478b19
Erkki Lepre <erkki@refined.com>
2022-02-18 13:00:52 +0200

    WIP

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Hello, World!! With Docker');
+  res.end('Hello, World!! With Docker 2');
 }
 
 const server = http.createServer(requestListener);
