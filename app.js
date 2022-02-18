c2107db579e0a213c91e618aa295755ae18b6092
Erkki Lepre <erkki@refined.com>
2022-02-18 14:50:31 +0200

    IP

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Hello, World!! Updated update 5');
+  res.end('Hello, World!! Updated update 6');
 }
 
 const server = http.createServer(requestListener);
